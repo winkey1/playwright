79689d1e6f62e09a8343fa226821ac3dba19f7a0
Restu <restu@example.com>
2025-09-28 11:09:22 +0700

    playwright

--- webrtc_control/data/content_script/page_context/additional_objects.js
+++
@@ -1,5 +0,0 @@
-{
-  if (typeof window.RTCDataChannel !== "undefined") window.RTCDataChannel = undefined;
-  if (typeof window.RTCIceCandidate !== "undefined") window.RTCIceCandidate = undefined;
-  if (typeof window.RTCConfiguration !== "undefined") window.RTCConfiguration = undefined;
-}
